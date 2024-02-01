82e1e67df7a749a95bebde4ad9d7fcdef2e1ab22
suyashkumarpandey <spandey@nextlooptechnologies.com>
2024-02-01 17:55:15 +0530

    dep 18

--- forge.config.ts
+++ forge.config.ts
@@ -18,11 +18,8 @@ const config: ForgeConfig = {
   ],
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
       build: [
         {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
           entry: "src/main.tsx",
           config: "vite.main.config.ts",
         },
