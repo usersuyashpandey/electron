4e71e2724fa9d3f076c0c11bee0f8a8681ca11cf
suyashkumarpandey <spandey@nextlooptechnologies.com>
2024-02-04 19:07:36 +0530

    version 1.0.10

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({
-  build: { chunkSizeWarningLimit: 1000000 },
+  build: { chunkSizeWarningLimit: 5000000 },
   resolve: {
     browserField: false,
     mainFields: ["module", "jsnext:main", "jsnext"],
