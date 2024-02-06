ae4966fa95ec69aab5be827f0c4d53d94ee1a6f9
usersuyashpandey <suyashpandey152@gmail.com>
2024-02-07 03:42:10 +0530

    4

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({
-  build: { chunkSizeWarningLimit: 5000000 },
+  build: {
+    chunkSizeWarningLimit: 5000000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   resolve: {
     browserField: false,
     mainFields: ["module", "jsnext:main", "jsnext"],
